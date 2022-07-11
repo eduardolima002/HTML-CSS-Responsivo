068153cbf46504e73250fbbb01a1206c1fa7b530
eduardolima002 <Eduardosalviano002@hotmail.com>
2022-07-11 18:15:20 -0300

    Projeto sem js

---
+++ js/carousel.js
@@ -0,0 +1,60 @@
+function Carousel(config) {
+    
+    this.container = (typeof config.container === 'string') ? 
+    document.querySelector(config.container) : config.container;
+    
+    this.itens = (typeof config.itens === 'string') ? 
+    document.querySelector(config.itens) : config.itens;
+    
+    this.btnPrev = (typeof config.btnPrev === 'string') ? 
+    document.querySelector(config.btnPrev) : config.btnPrev;
+    
+    this.btnNext = (typeof config.btnNext === 'string') ? 
+    document.querySelector(config.btnNext) : config.btnNext;
+
+ 
+
+    var _this = this;
+    var _currentSlide = 0;
+
+    init();
+
+    function init() {
+        var _show = _this.container.querySelectorAll('.show');
+
+        Array.prototype.forEach.call(_show, function(sh){
+            sh.classList.remove('show');
+        })
+
+        _this.itens[0].classList.add('show');
+        _this.btnNext.removeAttribute('style');
+        _this.btnPrev.removeAttribute('style');
+
+        addListeners();
+    }
+
+    function addListeners(){
+        _this.btnNext.addEventListener('click', showNextSlide);
+        _this.btnPrev.addEventListener('click', showPrevSlide);
+    }
+
+    function showNextSlide() {
+        _currentSlide++
+        showSlide()
+    }
+
+    function showPrevSlide(){
+        _currentSlide--;
+        showSlide();
+    }
+
+    function showSlide(){
+        var qtd = _this.itens.lenght;
+        var slide = _currentSlide % qtd;
+        slide = Math.abs(slide);
+
+        _this.container.querySelector('.show').classList.remove('show');
+        _this.itens[slide].classList.add('show');
+    }
+
+}
